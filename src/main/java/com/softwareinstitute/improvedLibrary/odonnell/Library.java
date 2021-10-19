1dea17bf81477f502797d98988f1dd36ee47a000
wodonnell98 <wodonnell@softwareinstitute.com>
2021-10-19 09:40:47 +0100

    Array list in

--- src/main/java/com/softwareinstitute/improvedLibrary/odonnell/Library.java
+++ src/main/java/com/softwareinstitute/improvedLibrary/odonnell/Library.java
@@ -1,4 +1,18 @@
 package com.softwareinstitute.improvedLibrary.odonnell;
 
+import java.util.ArrayList;
+
 public class Library {
+    public static void main(String[] args) {
+        ArrayList<String> libraryItems = new ArrayList<String>();
+        libraryItems.add("Book");
+        libraryItems.add("Computer");
+
+        //update element of arraylist
+        //libraryItems.set(1, "Asus");
+
+        for(String name: libraryItems) {
+            System.out.println(name);
+        }
+    }
 }
---
+++ src/test/java/com/softwareinstitute/improvedLibrary/odonnell/LibraryTest.java
@@ -0,0 +1,14 @@
+package com.softwareinstitute.improvedLibrary.odonnell;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LibraryTest {
+
+
+
+
+
+}
+
